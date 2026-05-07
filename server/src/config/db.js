639a0986ccd5744918afd0f077167396ae10e4dc
Gokul <mmgokulanthan491@gmail.com>
2026-05-07 08:21:30 +0530

    feat: improve MongoDB connection stability with increased timeouts

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,7 +4,10 @@ const logger = require('../utils/logger');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(config.mongoUri);
+    const conn = await mongoose.connect(config.mongoUri, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+    });
     logger.info(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     logger.error(`MongoDB connection error: ${err.message}`);
